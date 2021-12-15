6a1ab53ec827f2afe90b698a6a324a1a6e418a93
Benjamín Martinez <benjaminmq2013@gmail.com>
2021-12-15 13:13:24 -0300

    shopping cart is now Full Responsive Design

--- js/Shopping-cart/add_product.js
+++ js/Shopping-cart/add_product.js
@@ -20,24 +20,31 @@ function add_product(product_id){
     if (products[product_id].add == false){
                 
         let product_structure = (`
+        
         <div class="product_cart">
-            <img class="eliminate_btn" src="./icons/Property 1=delete-x.svg" alt="">
-            <img class="cart_product_image" src="${products[product_id].image_url}" alt="">
+            <div class="product_division">
+                <img class="eliminate_btn" src="./icons/Property 1=delete-x.svg" alt="">
+                <img class="cart_product_image" src="${products[product_id].image_url}" alt="">
 
-            <div class="product__cart_description">
-                <span class="cart_product_name">${products[product_id].product_name}</span>
-                <span class="cart_product_weight">$${products[product_id].product_price} USD${kilogramos}</span>
+                <div class="product__cart_description">
+                    <span class="cart_product_name">${products[product_id].product_name}</span>
+                    <span class="cart_product_weight">$${products[product_id].product_price} USD${kilogramos}</span>
+                </div>
             </div>
 
-            <div class="product_quantity_container">
-                <button onclick="amount_of_products(${products[product_id].prod_id}, ${subtract})" class="less_product_btn">-</button>
-                <input id="product_quantity_${products[product_id].prod_id}" class="product_quantity" type="text" value="1">
-                <button onclick="amount_of_products(${products[product_id].prod_id}, ${add})" class="more_product_btn">+</button>
-            </div>
+            
+            <div class="product_division product_details">
+                <div class="product_quantity_container">
+                    <button onclick="amount_of_products(${products[product_id].prod_id}, ${subtract})" class="less_product_btn">-</button>
+                    <input id="product_quantity_${products[product_id].prod_id}" class="product_quantity" type="text" value="1">
+                    <button onclick="amount_of_products(${products[product_id].prod_id}, ${add})" class="more_product_btn">+</button>
+                </div>
 
-            <div class="products_price_container">
-                <span id="total_${products[product_id].prod_id}" class="products_price">$26.82</span>
+                <div class="products_price_container">
+                    <span id="total_${products[product_id].prod_id}" class="products_price">$26.82</span>
+                </div>
             </div>
+            
 
         </div>
     `)
